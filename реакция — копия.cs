00031c2a009d13362fbe8a7c018950780455d19b
Vakhid <vahidabdulazizov@MacBook-Pro-Vahid.local>
2018-04-14 18:38:39 +0300

    все так ?

--- реакция — копия.cs
+++ реакция — копия.cs
@@ -11,19 +11,33 @@ namespace реакция
             Console.WriteLine("Вам надо будет ввести букву которую вы увидете на экране");
             Console.WriteLine("Жми Enter ");
             Console.ReadLine();
+            Random1;
+            Console.WriteLine();
+            Time1;
+            Console.WriteLine();
+        }
+    }
+    class Random1
+    {
+        public void ran ()
+        {
             Random randomizZnak = new Random();
             char[] Znak = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
             Thread.Sleep(randomizZnak.Next(1000, 5000));
-            DateTime time = DateTime.Now;
             char znakRandom = Znak[randomizZnak.Next(0, Znak.Length)];
             Console.WriteLine(znakRandom);
             while ((char)Console.ReadKey().KeyChar != znakRandom)
             {
             }
-            Console.WriteLine();
+        }
+    }
+    class Time1
+    {
+        public void Totime ()
+        {
+            DateTime time = DateTime.Now;
             TimeSpan timeCansel = DateTime.Now - time;
             Console.WriteLine($"Время реакции {timeCansel.TotalSeconds} сек");
-            Console.WriteLine();
         }
     }
 }
